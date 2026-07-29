f6029da0d925da67ffddb52497444eccfd39e498
M-logeshwaran <mg.logesh458@gmail.com>
2026-07-29 12:41:44 +0530

    Sync LeetCode solutions

---
+++ CODES/Easy/217.contains-duplicate.cpp
@@ -0,0 +1,48 @@
+// LeetCode 217. Contains Duplicate
+// Difficulty: Easy
+// Tags: Array, Hash Table, Sorting
+// Status: Accepted
+// Language: cpp
+// Runtime: 75
+// Memory: 111240000
+// URL: https://leetcode.com/problems/contains-duplicate/
+//
+// Question:
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+// Example 1:
+// Input:nums = [1,2,3,1]
+// Output:true
+// Explanation:
+// The element 1 occurs at the indices 0 and 3.
+// Example 2:
+// Input:nums = [1,2,3,4]
+// Output:false
+// Explanation:
+// All elements are distinct.
+// Example 3:
+// Input:nums = [1,1,1,3,3,4,3,2,4,2]
+// Output:true
+// Constraints:
+// - 1 <= nums.length <= 10^5
+// - -10^9 <= nums[i] <= 10^9
+//
+// Example Testcases:
+// [1,2,3,1]
+// [1,2,3,4]
+// [1,1,1,3,3,4,3,2,4,2]
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> map;
+        for(int i: nums){
+            if(map.contains(i)){
+                return true;
+            }
+            else{
+                map[i] += 1;
+            }
+        }
+        return false;
+    }
+};
